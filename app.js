0951c8815086e12006e1e4c773b4e08fe8ac6886
Geoffrey <geoffreyyang@gmail.com>
2021-05-04 17:37:18 -0400

    fixed capitalization

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const questionsRouter = require('./routes/questions');
-const askQuestionsRouter = require('./routes/askQuestions');
+const askQuestionsRouter = require('./routes/askquestions');
 const answersRouter = require('./routes/answers');
 const answerQuestionRouter = require('./routes/answerquestion');
 
