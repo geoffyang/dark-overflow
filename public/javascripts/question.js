985ad7997d73a81a27697ded83c0ac343ae9cbf9
Geoffrey <geoffreyyang@gmail.com>
2021-05-05 19:39:58 -0400

    EOD3

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -1,35 +1,20 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
     console.log("hello from questions.js!")
 
-    const upVote = document.querySelector("#fa-caret-square-up")
-    const downVote = document.querySelector("#fa-caret-square-down")
-    // const tempVote = document.querySelector('.upVoteButton')
+    const upVote = document.querySelector(".fa-caret-square-up")
+    const downVote = document.querySelector(".fa-caret-square-down")
 
-    upVote.addEventListener("click", e =>vote(1, e.target.id));
+    upVote.addEventListener("click", e => vote(1, e.target.id));
     downVote.addEventListener("click", e => vote(2, e.target.id));
 
-    // tempVote.addEventListener("click", (e) => {
-    //     const target = e.target;
-    //     const questionid = target.id;
-    //     console.log("Vote on q: this is the target", target)
-    //     console.log("Vote on q: this is the id", questionid);
-    //     vote(1, questionid)
-    // }
-    // );
 })
 
 async function vote(upOrDownCode, questionId) {
     try {
-        await fetch(`http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(upOrDownCode)}`, { method: `DELETE` })
-        await fetch(`http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(upOrDownCode)}`, { method: `POST` })
-        //TBD upVote button visual reaction:
+        await fetch(`http://localhost:8080/questions/${questionId}/vote`, { method: `DELETE` })
+        await fetch(`http://localhost:8080/questions/${questionId}/vote/${upOrDownCode}`, { method: `POST` })
+
+        //TBD add class to allow button visual reaction:
     }
     catch (err) { console.log("question vote error", err); };
 }
-
-// async function extractResponse(res) {
-//     if (!res.ok) throw res;
-//     const data = await res.json();
-//     console.log(`your vote has been counted`);
-//     return data;
-// }
--- routes/questions.js
+++ routes/questions.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const { asyncHandler } = require("./utils");
+const sequelize = require('sequelize');
 const { Question, Answer, QuestionVote, AnswerVote } = require('../db/models')
 const { check, validationResult } = require('express-validator');
 const { requireAuth } = require('../auth')
 
 // GET /questions/:id
 router.get('/:id', async (req, res, next) => {
-    const question = await Question.findByPk(parseInt(req.params.id, 10), {
+    const id = req.params.id
+    const question = await Question.findByPk(parseInt(id, 10), {
         include: [
             {
                 model: Answer,
@@ -23,24 +25,45 @@ router.get('/:id', async (req, res, next) => {
         ]
     });
 
-    console.log(question.dataValues.Answers)
-    res.render('question', {
-        question
+    const score = await QuestionVote.findAll({
+        attributes: [[sequelize.fn('sum', sequelize.col('voteSum')), 'total']],
+        where: { questionId: id }
     })
+    if (score[0].dataValues.total !== null) {
+        question.score = score[0].dataValues.total;
+    } else {
+        question.score = 0;
+    }
+    await question.save();
+    res.render('question', { question})
 })
 
-// GET /questions/:id/vote/votetype
+// POST /questions/:id/vote/votetype
 router.post('/:id(\\d+)/vote/:votetype(\\d+)', requireAuth, asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
-    const voteSum = parseInt(req.params.votetype, 10);
-    if (voteSum === 2) voteSum = -1;
+    let voteSum = parseInt(req.params.votetype, 10);
+    if (voteSum === 2) {
+        voteSum = -1;
+    }
     const { userId } = req.session.auth;
     const vote = await QuestionVote.create({ userId, questionId, voteSum });
+
     res.send();
 }));
 
-// DELETE /questions/:id/vote/votetype
-// tbd by Jimmy
+// DELETE /questions/:id/vote
+router.delete('/:id(\\d+)/vote', requireAuth, asyncHandler(async (req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const { userId } = req.session.auth;
+    const vote = await QuestionVote.findOne({
+        where: {
+            questionId,
+            userId
+        }
+    });
+    vote.destroy();
+    res.send();
+}));
 
 
 module.exports = router;
