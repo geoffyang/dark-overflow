6ab13a671241eaa9bf471d5a7d82f5d4f27be0f0
james <jodm522@gmail.com>
2021-05-05 15:17:26 -0500

    finish delete button

--- routes/questions.js
+++ routes/questions.js
@@ -1,33 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // const { csrfProtection, asyncHandler, } = require("./utils");
 // const bcrypt = require('bcryptjs');
-const { Question, Answer, QuestionVote, AnswerVote } = require('../db/models');
+const { Question, Answer, QuestionVote, AnswerVote } = require("../db/models");
 // const { check, validationResult } = require('express-validator');
 // const { loginUser, logoutUser } = require('../auth')
+const { requireAuth } = require("../auth");
+const { asyncHandler } = require("./utils");
 
-router.get('/:id', async (req, res, next) => {
-    const question = await Question.findByPk(parseInt(req.params.id, 10), {
+router.get("/:id", async (req, res, next) => {
+  const question = await Question.findByPk(parseInt(req.params.id, 10), {
+    include: [
+      {
+        model: Answer,
         include: [
-            {
-                model: Answer, 
-                include: [
-                    {
-                        model: AnswerVote
-                    }
-                ]
-            },
-            {
-                model: QuestionVote
-            }
-        ]
-    });
+          {
+            model: AnswerVote,
+          },
+        ],
+      },
+      {
+        model: QuestionVote,
+      },
+    ],
+  });
 
-    console.log(question.dataValues.Answers)
-    res.render('question', {
-        question
-    })
-})
+  let isUser;
+  const { userId } = req.session.auth;
+  if (userId === question.userId) isUser = true;
 
+  console.log(question.dataValues.Answers);
+  res.render("question", {
+    isUser,
+    question,
+  });
+});
+
+router.delete("/:id",
+   requireAuth,
+  asyncHandler(async (req, res) => {
+    const question = await Question.findByPk(parseInt(req.params.id, 10),{
+          include: [
+      {
+        model: Answer,
+        include: [
+          {
+            model: AnswerVote,
+          },
+        ],
+      },
+      {
+        model: QuestionVote,
+      },
+    ],
+  });
+    const { userId } = req.session.auth;
+    question.destroy();
+    res.send();
+  })
+);
 
 module.exports = router;
