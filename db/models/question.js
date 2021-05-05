07886fef86b280c994efa047d150dd0730ba222e
Geoffrey <geoffreyyang@gmail.com>
2021-05-04 20:38:56 -0400

    completed day 2

--- db/models/question.js
+++ db/models/question.js
@@ -3,14 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Question = sequelize.define('Question', {
     title: DataTypes.STRING,
     text: DataTypes.TEXT,
-    score: DataTypes.INTEGER,
+    score: DataTypes.INTEGER, 
     userId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER
   }, {});
   Question.associate = function(models) {
     Question.belongsTo (models.Profile, {foreignKey: 'userId'})
     Question.belongsTo (models.Category, {foreignKey: 'categoryId'})
-    Question.hasMany (models.QuestionVote, {foreignKey: 'questionId'})    
+    Question.hasMany (models.QuestionVote, {foreignKey: 'questionId'})
   };
   return Question;
 };
