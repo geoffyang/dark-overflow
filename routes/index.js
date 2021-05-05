54a097201d58322e06d6287296b63d2c57e6bf54
Adam Lovett <adamlovett@alumni.usc.edu>
2021-05-05 12:31:08 -0700

    layout front page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const { asyncHandler } = require("./utils");
-const { Profile, Question } = require('../db/models')
+const { Profile, Question, Answer, QuestionVote, Category } = require('../db/models')
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res, next) => {
   res.set('Cache-Control', 'no-store')
 
   const questions = await Question.findAll({
-    include: Profile,
+    include: [Profile, Answer, QuestionVote, Category],
     order: [['score', 'DESC']],
     limit: 10
   })
