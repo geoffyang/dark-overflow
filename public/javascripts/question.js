e3b8e31fd42e33088a93a6fa21ac7fbde03fa7ef
james <jodm522@gmail.com>
2021-05-06 11:31:01 -0500

    work on delete buttons

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -1,35 +1,71 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
-    console.log("hello from questions.js!")
+  console.log("hello from questions.js!");
 
-    const upVote = document.querySelector("#fa-caret-square-up")
-    const downVote = document.querySelector("#fa-caret-square-down")
-    // const tempVote = document.querySelector('.upVoteButton')
+    const upVote = document.querySelector("#fa-caret-square-up");
+    const downVote = document.querySelector("#fa-caret-square-down");
+  const tempVote = document.querySelector('.upVoteButton')
+    // upVote.addEventListener("click", (e) => vote(1, e.target.id));
+    // downVote.addEventListener("click", (e) => vote(2, e.target.id));
 
-    upVote.addEventListener("click", e =>vote(1, e.target.id));
-    downVote.addEventListener("click", e => vote(2, e.target.id));
+  tempVote.addEventListener("click", (e) => {
+      const target = e.target;
+      const questionid = target.id;
+      console.log("Vote on q: this is the target", target)
+      console.log("Vote on q: this is the id", questionid);
+      vote(1, questionid)
+  }
+  );
 
-    // tempVote.addEventListener("click", (e) => {
-    //     const target = e.target;
-    //     const questionid = target.id;
-    //     console.log("Vote on q: this is the target", target)
-    //     console.log("Vote on q: this is the id", questionid);
-    //     vote(1, questionid)
-    // }
-    // );
-})
+
+
+  const deleteQuestion = document.querySelector(".delete-question-btn");
+
+  deleteQuestion.addEventListener("click", async (e) => {
+    const target = e.target;
+    const id = target.id;
+    await deleteItem("question", "questions", id, "");
+  });
+});
+
+
+
+const deleteItem = async function (type, route, id, reroute) {
+  console.groupCollapsed(id);
+
+  try {
+    const res = await fetch(`/${route}/${id}`, {
+      method: "DELETE",
+    });
+    window.alert(`${type} sucessfully deleted.`);
+    window.location.href = "http://localhost:8080/";
+  } catch (err) {
+    window.alert("error: " + err);
+  }
+};
 
 async function vote(upOrDownCode, questionId) {
     try {
-        await fetch(`http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(upOrDownCode)}`, { method: `DELETE` })
-        await fetch(`http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(upOrDownCode)}`, { method: `POST` })
-        //TBD upVote button visual reaction:
+      await fetch(
+        `http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(
+          upOrDownCode
+        )}`,
+        { method: `DELETE` }
+      );
+      await fetch(
+        `http://localhost:8080/questions/${parseInt(questionId)}/vote/${parseInt(
+          upOrDownCode
+        )}`,
+        { method: `POST` }
+      );
+      //TBD upVote button visual reaction:
+    } catch (err) {
+      console.log("question vote error", err);
     }
-    catch (err) { console.log("question vote error", err); };
-}
+  }
 
-// async function extractResponse(res) {
-//     if (!res.ok) throw res;
-//     const data = await res.json();
-//     console.log(`your vote has been counted`);
-//     return data;
-// }
+  async function extractResponse(res) {
+      if (!res.ok) throw res;
+      const data = await res.json();
+      console.log(`your vote has been counted`);
+      return data;
+  }
--- routes/questions.js
+++ routes/questions.js
@@ -3,42 +3,12 @@ var router = express.Router();
 const { Question, Answer, QuestionVote, AnswerVote } = require("../db/models");
 const { requireAuth } = require("../auth");
 const { asyncHandler } = require("./utils");
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 // GET /questions/:id
-router.get('/:id', async (req, res, next) => {
-    const question = await Question.findByPk(parseInt(req.params.id, 10), {
-        include: [
-            {
-                model: Answer,
-                include: [
-                    {
-                        model: AnswerVote
-                    }
-                ]
-            },
-            {
-                model: QuestionVote
-            }
-        ]
-    });
-
-  let isUser;
-  const { userId } = req.session.auth;
-  if (userId === question.userId) isUser = true;
-
-  console.log(question.dataValues.Answers);
-  res.render("question", {
-    isUser,
-    question,
-  });
-});
-
-router.delete("/:id",
-   requireAuth,
-  asyncHandler(async (req, res) => {
-    const question = await Question.findByPk(parseInt(req.params.id, 10),{
-          include: [
+router.get("/:id", async (req, res, next) => {
+  const question = await Question.findByPk(parseInt(req.params.id, 10), {
+    include: [
       {
         model: Answer,
         include: [
@@ -52,23 +22,57 @@ router.delete("/:id",
       },
     ],
   });
+
+  let isQuestionAsker;
+  const { userId } = req.session.auth;
+  if (userId === question.userId) isQuestionAsker = true;
+
+  console.log(question.dataValues.Answers);
+  res.render("question", {
+    isQuestionAsker,
+    question,
+  });
+});
+
+router.delete(
+  "/:id",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const question = await Question.findByPk(parseInt(req.params.id, 10), {
+      include: [
+        {
+          model: Answer,
+          include: [
+            {
+              model: AnswerVote,
+            },
+          ],
+        },
+        {
+          model: QuestionVote,
+        },
+      ],
+    });
     const { userId } = req.session.auth;
     question.destroy();
     res.send();
   })
 );
 // GET /questions/:id/vote/votetype
-router.post('/:id(\\d+)/vote/:votetype(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+router.post(
+  "/:id(\\d+)/vote/:votetype(\\d+)",
+  requireAuth,
+  asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
     const voteSum = parseInt(req.params.votetype, 10);
     if (voteSum === 2) voteSum = -1;
     const { userId } = req.session.auth;
     const vote = await QuestionVote.create({ userId, questionId, voteSum });
     res.send();
-}));
+  })
+);
 
 // DELETE /questions/:id/vote/votetype
 // tbd by Jimmy
-
 
 module.exports = router;
