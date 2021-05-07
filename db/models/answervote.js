1e4b5e5b72129763f79e35def04f6360a28b8f8c
james <jodm522@gmail.com>
2021-05-07 15:23:39 -0500

    work on seeders

--- db/models/answervote.js
+++ db/models/answervote.js
@@ -1,13 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const AnswerVote = sequelize.define('AnswerVote', {
-    voteSum: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    answerId: DataTypes.INTEGER
-  }, {});
-  AnswerVote.associate = function(models) {
-    AnswerVote.belongsTo (models.Profile, {foreignKey: 'userId'})
-    AnswerVote.belongsTo (models.Answer, {foreignKey: 'answerId'}) 
+  const AnswerVote = sequelize.define(
+    "AnswerVote",
+    {
+      voteSum: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+      answerId: DataTypes.INTEGER,
+    },
+    {}
+  );
+  AnswerVote.associate = function (models) {
+    AnswerVote.belongsTo(models.Profile, { foreignKey: "userId" });
+    AnswerVote.belongsTo(models.Answer, { foreignKey: "answerId" });
   };
   return AnswerVote;
 };
--- db/models/profile.js
+++ db/models/profile.js
@@ -1,23 +1,50 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Profile = sequelize.define('Profile', {
-    userName: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING.BINARY,
-    needsJob: DataTypes.BOOLEAN,
-    offeringJob: DataTypes.BOOLEAN,
-    score: DataTypes.INTEGER
-  }, {});
-  Profile.associate = function(models) {
-    Profile.hasMany(models.Question, { foreignKey: 'userId'})
-    Profile.hasMany(models.Answer, { foreignKey: 'userId'})
-    Profile.hasMany(models.QuestionVote, { foreignKey: 'userId'})
-    Profile.hasMany(models.AnswerVote, { foreignKey: 'userId'})
-    Profile.hasMany(models.CommentVote, { foreignKey: 'userId'})
-    Profile.hasMany(models.Comment, { foreignKey: 'userId'})
-
+  const Profile = sequelize.define(
+    "Profile",
+    {
+      userName: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      hashedPassword: DataTypes.STRING.BINARY,
+      needsJob: DataTypes.BOOLEAN,
+      offeringJob: DataTypes.BOOLEAN,
+      score: DataTypes.INTEGER,
+    },
+    {}
+  );
+  Profile.associate = function (models) {
+    Profile.hasMany(models.Question, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+    Profile.hasMany(models.Answer, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+    Profile.hasMany(models.QuestionVote, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+    Profile.hasMany(models.AnswerVote, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+    Profile.hasMany(models.CommentVote, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+    Profile.hasMany(models.Comment, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      hooks: true,
+    });
   };
   return Profile;
 };
