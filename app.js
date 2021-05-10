5a36659fc67329f72cf11ae27e9c43028cc6d4b5
Adam Lovett <adamlovett@alumni.usc.edu>
2021-05-09 23:33:50 -0700

    small fix

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ const searchRouter = require('./routes/searchresults')
 const { restoreUser, requireAuth } = require('./auth.js')
 
 const app = express();
-app.use(cors);
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 // view engine setup
 app.set('view engine', 'pug');
 
